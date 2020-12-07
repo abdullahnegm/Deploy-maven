90c3096140bcf4efe5686ac3e374f5e7e5d77134
abdullah <abdullahahmed1997@hotmail.com>
2020-12-07 18:11:34 +0200

    edit

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run --rm -v "$PWD/java-app:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
+
+WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/maven-job
+
+
+docker run --rm -v "$WORKSPACE/java-app:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
