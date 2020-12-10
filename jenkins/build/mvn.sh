d59dfa49f959f455c6fe2744d76a41390fb98d09
abdullah <abdullahahmed1997@hotmail.com>
2020-12-10 13:03:51 +0200

    more learning

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/maven-job
+WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/pipe-maven
 
-docker run --rm -v "$WORKSPACE:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
+docker run --rm -v "$WORKSPACE/java-app:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/maven-job
+WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/pipe-maven
 
 
-docker run --rm -v "$WORKSPACE/java-app:/app" -v '/root/.m2:/root/.m2' -w /app maven:3-alpine "$@"
+docker run --rm -v "$WORKSPACE/jave-app:/app" -v '/root/.m2:/root/.m2' -w /app maven:3-alpine "$@"
