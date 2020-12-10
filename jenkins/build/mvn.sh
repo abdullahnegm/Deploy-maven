6fa7063f5826d709842e16f03193adf16624f096
abdullah <abdullahahmed1997@hotmail.com>
2020-12-10 12:19:36 +0200

    Learn more

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,4 +3,4 @@
 
 WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/maven-job
 
-docker run --rm -v "$WORKSPACE/java-app:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
+docker run --rm -v "$WORKSPACE:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
