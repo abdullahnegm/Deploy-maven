d9e0ecf2920376214be5d6e8fd7fa1bf5650e98f
abdullah <abdullahahmed1997@hotmail.com>
2020-12-10 19:40:22 +0200

    more more

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,4 +4,4 @@
 WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/pipe-maven
 
 
-docker run --rm -v "$WORKSPACE/jave-app:/app" -v '/root/.m2:/root/.m2' -w /app maven:3-alpine "$@"
+docker run --rm -v "$WORKSPACE:/app" -v '/root/.m2:/root/.m2' -w /app maven:3-alpine "$@"
