1e0a543590f2ed123ea76d2047d4f4ed752114da
abdullah <abdullahahmed1997@hotmail.com>
2020-12-11 13:58:50 +0200

    try

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,4 +3,4 @@
 
 WORKSPACE=/home/jenkins/jenkins/jenkins-data/jenkins_home/workspace/pipe-maven
 
-docker run --rm -v "$WORKSPACE/java-app:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
+docker run --rm -v "/var/jenkins_home/workspace/pipe-maven/java-app:/app" -v "/root/.m2:/root/.m2" -w /app maven:3-alpine "$@"
